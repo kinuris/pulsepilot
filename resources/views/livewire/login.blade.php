fda4991d57fd10c8a3776eefd655c8e1fa072cac
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2025-04-11 06:10:41 +0800

    Added loading spinner

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -49,8 +49,17 @@
 
             <button
                 type="submit"
-                class="w-full flex justify-center py-3 px-4 border border-transparent rounded-lg shadow-sm text-white bg-[#294249] hover:bg-[#7aa0ab] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#7aa0ab] transition-all duration-200 text-sm font-medium">
-                Sign In
+                class="w-full flex justify-center py-3 px-4 border border-transparent rounded-lg shadow-sm text-white bg-[#294249] hover:bg-[#7aa0ab] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#7aa0ab] transition-all duration-200 text-sm font-medium"
+                wire:loading.attr="disabled">
+                <span wire:loading.remove>
+                    Sign In
+                </span>
+                <span wire:loading>
+                    <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                </span>
             </button>
         </form>
     </div>
