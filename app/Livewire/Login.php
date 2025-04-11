f088433b46cd8e45f0a742470fcfc04ac70f6d54
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2025-04-11 09:11:45 +0800

    Changes

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -38,12 +38,14 @@ class Login extends Component
             return back()->with('error', 'Invalid login details');
         }
 
-        if (User::query()->where('email', '=', $this->email)->first()->suspended !== null) {
-            return back()->with('error', 'Your account has been suspended by Admin. Please contact support.');
-        }
-
         if (!Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
             return back()->with('error', 'Invalid login details');
+        }
+
+        if (User::query()->where('email', '=', $this->email)->first()->suspended !== null) {
+            Auth::logout();
+
+            return back()->with('error', 'Your account has been suspended by Admin. Please contact support.');
         }
 
         $user = Auth::user();
